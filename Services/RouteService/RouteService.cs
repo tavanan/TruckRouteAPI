46c8c1f0a9447df9a8cc38407a7082100ee3981f
tavanan <nadertavana888@gmail.com>
2021-12-13 11:06:39 +0100

    Add Map method to the RouteService

--- Services/RouteService/RouteService.cs
+++ Services/RouteService/RouteService.cs
@@ -18,6 +18,23 @@ namespace TruckRouteAPI.Services.RouteService
             new Country("CRI"),
             new Country("PAN")
         };
+        
+        /// <summary>
+        /// Add neighbours to the countries to make a map
+        /// </summary>
+        private void Map()
+        {
+            
+            countries[0].AddNeighbours(countries[1], countries[2]);
+            countries[2].AddNeighbours(countries[3], countries[4]);
+            countries[3].AddNeighbours(countries[4]);
+            countries[4].AddNeighbours(countries[5], countries[6]);
+            countries[5].AddNeighbours(countries[6]);
+            countries[6].AddNeighbours(countries[7]);
+            countries[7].AddNeighbours(countries[8]);
+            countries[8].AddNeighbours(countries[9]);
+
+        }
 
     }
 }
