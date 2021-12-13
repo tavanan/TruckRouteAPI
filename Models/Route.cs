76258d09eb465f4c2a3c7989d0cacbb6eca7da85
tavanan <nadertavana888@gmail.com>
2021-12-13 10:51:21 +0100

    Add Route model

---
+++ Models/Route.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace TruckRouteAPI.Models
+{
+    ///<remarks>Hold data for response</remarks>
+    public class Route
+    {
+        ///<summary>
+        /// Destination Country code
+        /// </summary> 
+        public string Destination { get; set; } = null;
+
+        /// <summary>
+        /// Countries on route to the destination
+        /// </summary>
+        public List<string> Countries { get; set; } = new List<string>();
+    }
+}
