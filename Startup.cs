f71eab9a7c02e7c77d877bb129a6c69262fa79b7
tavanan <nadertavana888@gmail.com>
2021-12-13 11:20:28 +0100

    Add RouteService to the service container

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TruckRouteAPI.Services.RouteService;
 
 namespace TruckRouteAPI
 {
@@ -26,6 +27,7 @@ namespace TruckRouteAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<IRouteService,RouteService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
