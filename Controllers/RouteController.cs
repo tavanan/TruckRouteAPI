e31da33cfabfdc6503838e9cd75c75b6024e0edd
tavanan <nadertavana888@gmail.com>
2021-12-13 12:13:00 +0100

    Edit Ilogger

--- Controllers/RouteController.cs
+++ Controllers/RouteController.cs
@@ -16,8 +16,8 @@ namespace TruckRouteAPI.Controllers
     public class RouteController : ControllerBase
     {
         private readonly IRouteService _routeService;
-        private ILogger _logger;
-        public RouteController(IRouteService routeService, ILogger logger)
+        private readonly ILogger<RouteController> _logger;
+        public RouteController(IRouteService routeService, ILogger<RouteController> logger)
         {
             _routeService = routeService;
             _logger = logger;
@@ -30,37 +30,7 @@ namespace TruckRouteAPI.Controllers
         {
             try
             {
-                return Ok(@"To get the Route from USA, add 3 letters CountryCode at the end : 
-                          truckrouteapi.azurewebsites.net/{CountryCode}");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went wrong: {ex}");
-                return StatusCode(500, "Internal server error");
-            }
-
-        }
-
-
-        [HttpGet("{id}")]
-        public ActionResult<ServiceResponse<Route>> GetById(string id)
-        {
-            try
-            {
-                var serviceResponse = _routeService.GetRouteFromUSA(id);
-                if (id.Length != 3)
-                {
-                    return BadRequest(serviceResponse);
-                }
-                else if (serviceResponse.Success == false)
-                {
-                    return NotFound(serviceResponse);
-                }
-                else
-                {
-                    return Ok(serviceResponse);
-                }
-
+                return Ok(@"To get the Route from USA, add 3 letters CountryCode at the end : truckrouteapi.azurewebsites.net/{CountryCode}");
             }
             catch (Exception ex)
             {
